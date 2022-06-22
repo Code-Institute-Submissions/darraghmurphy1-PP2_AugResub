e4cc3d498696050191273f0acd1b171ebb86ecf8
darraghmurphy1 <darraghmurphy77@gmail.com>
2022-06-22 21:02:12 +0000

    changed question style and added functions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
+const question = document.getElementsById('question');
+const choices = array.from(document.getElementsByClassName('answer-text'));
+
+let thisQuestion = {}
+let correctAnswer = true
+let startingScore = 0
+let questionTrack = 0
+let theQuestions = []
+
+
+
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
 let answerButtons = document.getElementsByClassName('answer-btn');
-let startingScore = 0;
+
 let startingIncorrectScore=0;
 let currentQuestionIndex=0;
 
@@ -12,17 +23,55 @@ let currentQuestionIndex=0;
 document.startButton.addEventListener('click', startQuiz);
 
 function startQuiz () {
-  startButton.classList.add('hidden')
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hidden')
+  questionTrack=0
+  startingScore=0
+  theQuestions= [...questions]
   nextQuestion()
 }
 
 function nextQuestion () {
+  const questionsIndex = Math.floor(Math.random() * theQuestions.length)
+  thisQuestion =  theQuestions[questionsIndex]
+  question.innerText = thisQuestion.question
+  
+  answers.forEach(answer => {
+    const number = choice.dataset['number']
+    answer.innerText = thisQuestion ['answer' + number]
 
+    theQuestions.splice(questionsIndex, 1)
+
+    acceptedAnswers = true
+
+    answers.forEach(answer => {
+      answer.addEventListener('click' e => {
+        if(!acceptedAnswers) return
+
+        acceptedAnswers = false 
+        const selectedOption = e.target
+        const selectedAnswer = selectedOption.dataset['number']
+        
+
+        let classToApply = selectedAnswer == thisQuestion.answer ? 'correct' : 'incorrect'
+
+        if (classToApply=== 'correct') {
+        
+          incrementScore(SCORE_POINTS)  /* not defined yet */
+        }
+
+        selectedOption.parentElement.classList.add(classToApply)
+
+        setTimeout (() => {
+          selectedOption.parentElement.classList.remove(classToApply)
+          newQuestion()
+        }, 1000)
+
+  })
+    })
+
+  })
 };
 
-function displayQuestion () {
+function newQuestion () {
 
 };
 
@@ -30,7 +79,7 @@ function checkAnswer () {
 
 };
 
-function incrementScore() {
+function incrementScore()= num =>  {
 
 };
 
@@ -48,141 +97,145 @@ function retry () {
 
 /* questions for the quiz */
 
-const questions = [
+let questions = [
 
-    {
-      question: 'What was the name of the French football player who was infamously sent off in the 2006 World Cup Final?',
-      answers: [
-        { text: 'Zinedine Zidane', correct: true },
-        { text: 'William Gallas', correct: false },
-        { text: 'Frank Ribery', correct: false},
-        { text: 'Thierry Henry',correct: false}
-      ] 
-    },
+  {
+    question: 'What was the name of the French football player who was infamously sent off in the 2006 World Cup Final?',
+
+       option1: 'Zinedine Zidane',
+       option2: 'William Gallas',
+       option3: 'Frank Ribery', 
+       option4: 'Thierry Henry',
+       answer: 1,
     
+  },
   
-    {
-      question: 'The only city to have hosted both the Summer and Winter Olympic Games is which of the following?',
-      answers: [
-        { text: 'Beijing', correct: true },
-        { text: 'Moscow', correct: false },
-        { text: 'Helsinki', correct: false},
-        { text: 'Montreal',correct: false}
-      ] 
-    },
-    {
-      question: 'Who was the first team to win the FA Cup?',
-      answers: [
-        { text: 'Wanderers', correct: true },
-        { text: 'Maidenhead', correct: false },
-        { text: 'Queens Park', correct: false},
-        { text: 'Crystal Palace',correct: false}
-      ] 
-    },
-    {
-      question: 'Which has won the most Olympic medals in total?',
-      answers: [
-        { text: 'United States', correct: true },
-        { text: 'United Kingdom', correct: false },
-        { text: 'France', correct: false},
-        { text: 'China',correct: false}
-      ] 
-    },
-    {
-      question: 'Who won the very first UFC tournament (now known as UFC 1)?',
-      answers: [
-        { text: 'Royce Gracie', correct: true },
-        { text: 'Ken Shamrock', correct: false },
-        { text: 'Art Jimmerson', correct: false},
-        { text: 'Gerard Gordeau',correct: false}
-      ] 
-    },
-    {
-      question: 'Who did Michael Schumacher win his first F1 World Drivers Championship?',
-      answers: [
-        { text: 'Benetton', correct: true },
-        { text: 'Jordan', correct: false },
-        { text: 'Minardi', correct: false},
-        { text: 'Ferrari',correct: false}
-      ] 
-    },
-    {
-      question: 'Which tennis player has spent the longest amount of time at rank number 1 in mens?',
-      answers: [
-        { text: 'Roger Federer', correct: true },
-        { text: 'Novak Djokovic', correct: false },
-        { text: 'Rafael Nadal', correct: false},
-        { text: 'Pete Sampras',correct: false}
-      ] 
-    },
-    {
-      question: '"Who has won the most GAA All-Ireland Football Championships in a row?',
-      answers: [
-        { text: 'Dublin', correct: true },
-        { text: 'Cork', correct: false },
-        { text: 'Fermanagh', correct: false},
-        { text: 'Kerry',correct: false}
-      ] 
-    },
-  
-    {
-      question: 'Which is the smallest area by population represented by a football team to win the European Cup/Champions League?',
-      answers: [
-        { text: 'Porto', correct: true },
-        { text: 'Nottingham', correct: false },
-        { text: 'Belgrade', correct: false},
-        { text: 'Salzburg',correct: false}
-      ] 
-    },
-    {
-      question: 'Who is the only football player to have scored a goal in the Premier League, Championship, League One, League Two, FA Cup, League Cup, Football League Trophy, FA Trophy, UEFA Champions League, UEFA Europa League, Scottish Cup, Scottish League Cup and the Scottish Premier League?',
-      answers: [
-        { text: 'Gary Hooper', correct: true },
-        { text: 'Steve Davis', correct: false },
-        { text: 'Billy Sharp', correct: false},
-        { text: 'Tony Watt',correct: false}
-      ] 
-    },
+
+  {
+    question: 'The only city to have hosted both the Summer and Winter Olympic Games is which of the following?',
     
+      option1: 'Helsinki',
+      option2: 'Moscow',
+      option3: 'Beijing', 
+      option4: 'Montreal',
+      answer: 3,
+  },
+  {
+    question: 'Who was the first team to win the FA Cup?',
+    
+      option1: 'Maidenhead',
+      option2: 'Wanderers',
+      option3: 'Queens Park',
+      option4: 'Crystal Palace',
+      answer: 2,
+  },
+  {
+    question: 'Which has won the most Olympic medals in total?',
+   
+      option1: 'United States', 
+      option2: 'United Kingdom', 
+      option3: 'France', 
+      option4: 'China',
+      answer: 1,
+  },
+  {
+    question: 'Who won the very first UFC tournament (now known as UFC 1)?',
+    
+      option1: 'Gerard Gordeau',
+      option2: 'Ken Shamrock',
+      option3: 'Art Jimmerson',
+      option4: 'Royce Gracie',
+      answer: 4,
+    
+  },
+  {
+    question: 'Who did Michael Schumacher win his first F1 World Drivers Championship?',
   
-    {
-      question: 'Who has won the most Golden Boot awards in the Italian top football division, Serie A?',
-      answers: [
-        { text: 'Gunnar Nordahl', correct: true },
-        { text: 'Michel Platini', correct: false },
-        { text: 'Ciro Immobile', correct: false},
-        { text: 'Zlatan Ibrahimović',correct: false}
-      ] 
-    },
-    {
-      question: 'In American Football, who is the Superbowl trophy named after?',
-      answers: [
-        { text: 'Vince Lombardi', correct: true },
-        { text: 'Marv Levy', correct: false },
-        { text: 'George Allen', correct: false},
-        { text: 'Paul Brown',correct: false}
-      ] 
-    },
+      option1: 'Benetton',
+      option2: 'Jordan', 
+      option3: 'Minardi',
+      option4: 'Ferrari',
+      answer: 1,
+  },
+  {
+    question: 'Which tennis player has spent the longest amount of time at rank number 1 in mens?',
+    
+    option1: 'Novak Djokovic',
+    option2: 'Roger Federer',
+    option3: 'Rafael Nadal', 
+    option4: 'Pete Sampras',
+    answer: 2,
+    
+  },
+  {
+    question: '"Who has won the most GAA All-Ireland Football Championships in a row?',
+    
+    option1: 'Dublin',
+    option2: 'Cork',
+    option3: 'Fermanagh',
+    option4: 'Kerry',
+    answer:1 ,
+  },
+
+  {
+    question: 'Which is the smallest area by population represented by a football team to win the European Cup/Champions League?',
+    
+    option1: 'Nottingham',
+    option2: 'Porto', 
+    option3: 'Belgrade',
+    option4: 'Salzburg',
+    answer: 2,
+    
+  },
+  {
+    question: 'Who is the only football player to have scored a goal in the Premier League, Championship, League One, League Two, FA Cup, League Cup, Football League Trophy, FA Trophy, UEFA Champions League, UEFA Europa League, Scottish Cup, Scottish League Cup and the Scottish Premier League?',
   
-    {
-      question: 'The famous boxing match between Muhammad Ali and George Foreman known as "The Rumble in the Jungle" took place where?',
-      answers: [
-        { text: 'Kinshasa, Zaire', correct: true },
-        { text: 'Lagos, Nigeria', correct: false },
-        { text: 'Libreville, Gabon', correct: false},
-        { text: 'Windhoek, Namibia',correct: false}
-      ] 
-    },
+      option1: 'Tony Watt', 
+      option2: 'Steve Davis',
+      option3: 'Billy Sharp',
+      option4: 'Gary Hooper',
+      answer: 4,
+  },
   
-    {
-      question: 'Who was the first footballer to be bought for over £10,000,000?',
-      answers: [
-        { text: 'Jean-Pierre Papin', correct: true },
-        { text: 'Alan Shearer', correct: false },
-        { text: 'Ronaldo', correct: false},
-        { text: 'Gianluca Vialli',correct: false}
-      ] 
-    },
+
+  {
+    question: 'Who has won the most Golden Boot awards in the Italian top football division, Serie A?',
   
-  
-  ]
+      option1: 'Gunnar Nordahl', 
+      option2: 'Michel Platini', 
+      option3: 'Ciro Immobile', 
+      option4: 'Zlatan Ibrahimović',
+      answer: 1,
+  },
+  {
+    question: 'In American Football, who is the Superbowl trophy named after?',
+    
+      option1: 'George Allen', 
+      option2: 'Marv Levy', 
+      option3: 'Vince Lombardi', 
+      option4: 'Paul Brown',
+      answer: 3,
+  },
+
+  {
+    question: 'The famous boxing match between Muhammad Ali and George Foreman known as "The Rumble in the Jungle" took place where?',
+    
+      option1: 'Windhoek, Namibia',
+      option2: 'Lagos, Nigeria',
+      option3: 'Libreville, Gabon', 
+      option4: 'Kinshasa, Zaire',
+      answer: 4,
+  },
+
+  {
+    question: 'Who was the first footballer to be bought for over £10,000,000?',
+    
+      option1: 'Jean-Pierre Papin',
+      option2: 'Alan Shearer', 
+      option3: 'Ronaldo', 
+      option4: 'Gianluca Vialli',
+      answer: 1,
+  },
+
+
+]
